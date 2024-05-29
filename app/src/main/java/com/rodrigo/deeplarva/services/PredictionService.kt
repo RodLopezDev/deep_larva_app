dc543acd4bf9cbc0512461004493472745886356
RodLopezDev <rodrigolopezdev@gmail.com>
2024-05-28 22:36:08 -0500

    feat: :sparkles: added background task

--- app/src/main/java/com/rodrigo/deeplarva/services/PredictionService.kt
+++ app/src/main/java/com/rodrigo/deeplarva/services/PredictionService.kt
@@ -10,9 +10,18 @@ import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.rodrigo.deeplarva.R
+import com.rodrigo.deeplarva.domain.entity.Picture
+import com.rodrigo.deeplarva.domain.entity.SubSample
+import com.rodrigo.deeplarva.infraestructure.DbBuilder
+import com.rodrigo.deeplarva.infraestructure.driver.AppDatabase
 import com.rodrigo.deeplarva.routes.PicturesActivity
+import com.rodrigo.deeplarva.routes.services.PicturesServices
+import com.rodrigo.deeplarva.routes.services.SubSampleServices
+import com.rodrigo.deeplarva.ui.tasks.BackgroundTaskPredict
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -30,6 +39,14 @@ class PredictionService: Service() {
     private val binder = LocalBinder()
     private val sender = PredictionBroadcastSender(this)
 
+
+    private var backgroundTask = BackgroundTaskPredict(this)
+
+
+    private lateinit var db: AppDatabase
+    private lateinit var pictureService: PicturesServices
+    private lateinit var subSampleService: SubSampleServices
+
     override fun onBind(intent: Intent?): IBinder? {
         var subSampleId = intent?.getLongExtra("subSampleId", 0)
         Log.d(TAG, "onBind: ${subSampleId}")
@@ -40,11 +57,17 @@ class PredictionService: Service() {
         super.onCreate()
         Log.d(TAG, "Service Created")
         createNotificationChannel()
+
+        db = DbBuilder.getInstance(this)
+
+        pictureService = PicturesServices(db)
+        subSampleService = SubSampleServices(db)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "Service Started")
-        var subSampleId = intent?.getLongExtra("subSampleId", 0)
+        var subSampleId = intent?.getLongExtra("subSampleId", 0) ?: 0
         Log.d(TAG, "onStartCommand: ${subSampleId}")
         if(subSampleId?.toInt() == 0){
             this.onDestroy()
@@ -65,24 +88,106 @@ class PredictionService: Service() {
             .build()
 
         startForeground(NOTIFICATION_ID, notification)
+//
+//        sender.notify(1)
+//        GlobalScope.launch {
+//            delay(2000L)
+//            sender.notify(30)
+//            delay(2000L)
+//            sender.notify(60)
+//            delay(2000L)
+//            sender.notify(70)
+//            delay(10000L)
+//            sender.notify(90)
+//            withContext(Dispatchers.Main) {
+//                this@PredictionService.onDestroy()
+//            }
+//        }
 
-        sender.notify(1)
-        GlobalScope.launch {
-            delay(2000L)
-            sender.notify(30)
-            delay(2000L)
-            sender.notify(60)
-            delay(2000L)
-            sender.notify(70)
-            delay(10000L)
-            sender.notify(90)
-            withContext(Dispatchers.Main) {
-                this@PredictionService.onDestroy()
+        eventPredict(subSampleId)
+
+        return START_STICKY
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun eventPredict(subSampleId: Long) {
+        if(backgroundTask.isProcessing){
+            Toast.makeText(this, "Process is running yet", Toast.LENGTH_LONG).show()
+            return
+        }
+        pictureService.findUnprocessedBySubSampleId(subSampleId) {
+                pictures ->
+            if (pictures.isNotEmpty()){
+                Toast.makeText(this, "PROCESSING, ${pictures.size}", Toast.LENGTH_LONG).show()
+                backgroundTask.predictBatchCOROUTINE(
+                    subSampleId,
+                    pictures,
+                    ::eventUpdatePredictionProgress,
+                    ::eventEntityPredictionProgress,
+                    ::eventFinishPrediction
+                )
+            } else {
+                eventFinishPrediction(subSampleId)
             }
         }
+    }
 
-        // If we get killed, after returning from here, restart
-        return START_STICKY
+
+
+    private fun eventUpdatePredictionProgress(status: Int) {
+        sender.notify(status)
+    }
+
+    private fun eventEntityPredictionProgress(id: Long, counter: Int, bitmapProcessedPath: String, callback: () -> Unit) {
+        pictureService.findOne(id) {
+            if (it != null) {
+                pictureService.update(
+                    Picture(
+                        id = it.id,
+                        count = counter,
+                        filePath = it.filePath,
+                        subSampleId = it.subSampleId,
+                        hasMetadata = true,
+                        processedFilePath = bitmapProcessedPath,
+                        time = 0,
+                        thumbnailPath = it.thumbnailPath
+                    )
+                ) {
+                    callback()
+                }
+            }
+        }
+    }
+
+    private fun updateSubSampleInfo(subSampleId: Long, callback: () -> Unit) {
+        subSampleService.findOne(subSampleId) {
+            subSample -> run {
+            if(subSample != null) {
+                pictureService.findProcessedBySubSampleId(subSampleId) {
+                        pictures -> run {
+                    if(pictures.isNotEmpty()){
+                        val min = pictures.minOf { it.count }
+                        val max = pictures.maxOf { it.count }
+                        val valuesList = pictures.map { it.count }.distinct()
+                        val fashionCounts = valuesList.groupingBy { it }.eachCount()
+                        val mostCommonFashion = fashionCounts.maxByOrNull { it.value }?.key
+
+                        val updated = SubSample(id=subSample.id, isTraining = true, min = min.toFloat(), max = max.toFloat(), mean = mostCommonFashion?.toFloat() ?: 0f, average = 0f, name = subSample.name)
+                        subSampleService.update(updated) {
+                            callback()
+                        }
+                    }
+                }}
+            } else {
+                callback()
+            }
+        }}
+    }
+
+    private fun eventFinishPrediction(subSampleId: Long) {
+        updateSubSampleInfo(subSampleId) {
+            this.onDestroy()
+        }
     }
 
     override fun onDestroy() {
