df96fc4ca952dfe44b8480c23560900b5e52b371
RodLopezDev <rodrigolopezdev@gmail.com>
2024-06-08 02:05:26 -0500

    fix: :bug: removed code smells of bd

--- app/src/main/java/com/rodrigo/deeplarva/domain/entity/BoxDetection.kt
+++ app/src/main/java/com/rodrigo/deeplarva/domain/entity/BoxDetection.kt
@@ -3,6 +3,7 @@ package com.rodrigo.deeplarva.domain.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -14,7 +15,8 @@ import androidx.room.PrimaryKey
             childColumns = ["picture_id"],
             onDelete = ForeignKey.CASCADE
         )
-    ]
+    ],
+    indices = [Index(value = ["picture_id"])]
 )
 data class BoxDetection(
     @PrimaryKey(autoGenerate = true)
