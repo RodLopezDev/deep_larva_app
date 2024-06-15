e2f8e3f75e42542a303c34ee6eadbc806c1b8162
RodLopezDev <rodrigolopezdev@gmail.com>
2024-06-15 18:42:35 -0500

    feat: :sparkles: added circuitbreak for http service

--- app/src/main/java/com/rodrigo/deeplarva/application/usecases/UseCaseSyncPicture.kt
+++ app/src/main/java/com/rodrigo/deeplarva/application/usecases/UseCaseSyncPicture.kt
@@ -4,9 +4,9 @@ import com.rodrigo.deeplarva.domain.entity.Picture
 import com.rodrigo.deeplarva.domain.requests.SyncPictureRequest
 import com.rodrigo.deeplarva.domain.response.NewPictureResponse
 import com.rodrigo.deeplarva.infraestructure.services.BackendPictureServices
-import com.rodrigo.deeplarva.modules.requests.RequestListener
 import com.rodrigo.deeplarva.infraestructure.services.BoxDetectionServices
 import com.rodrigo.deeplarva.infraestructure.services.PicturesServices
+import com.rodrigo.deeplarva.modules.requests.RequestListener
 import java.io.File
 
 class UseCaseSyncPicture(
@@ -21,7 +21,6 @@ class UseCaseSyncPicture(
             listener.onFailure()
             return
         }
-
         boxDetectionServices.findByPictureId(picture.id) {boxes -> run {
             val objPayload = SyncPictureRequest(picture, boxes)
             backendPictureServices.saveSample(objPayload, object: RequestListener<NewPictureResponse> {
@@ -29,6 +28,10 @@ class UseCaseSyncPicture(
                     listener.onFailure()
                 }
                 override fun onComplete(response: NewPictureResponse) {
+                    if(response.circuitBreak == true){
+                        listener.onComplete("")
+                        return
+                    }
                     backendPictureServices.uploadFile(response, originalBitmap, object: RequestListener<Boolean> {
                         override fun onFailure() {
                             listener.onFailure()
