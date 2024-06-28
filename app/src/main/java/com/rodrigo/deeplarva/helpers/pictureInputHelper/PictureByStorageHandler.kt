8ae09924bd18a3ab38cf3d22bcb4f9f5c73bf7ac
Anderson <billbardales@gmail.com>
2024-06-28 11:28:38 -0500

    Revert "Add function to rotate images"
    This reverts commit 06e4b19abc567608f0c1a7a8ced2c230d23e5702.

--- app/src/main/java/com/rodrigo/deeplarva/helpers/pictureInputHelper/PictureByStorageHandler.kt
+++ app/src/main/java/com/rodrigo/deeplarva/helpers/pictureInputHelper/PictureByStorageHandler.kt
@@ -3,11 +3,7 @@ package com.rodrigo.deeplarva.helpers.pictureInputHelper
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Matrix
-import android.media.ExifInterface
-import android.net.Uri
 import android.provider.MediaStore
-import java.io.IOException
 
 class PictureByStorageHandler(override val activity: Activity): IPictureReceiverHandler {
     private val REQUESTCODE = 101
@@ -19,53 +15,11 @@ class PictureByStorageHandler(override val activity: Activity): IPictureReceiver
     }
     override fun getBitmap(requestCode: Int, resultCode: Int, data: Intent?): List<Bitmap> {
         if (requestCode == REQUESTCODE && resultCode == Activity.RESULT_OK){
-            val uri = data?.data ?: throw Exception("ERROR_GETTING_IMAGE")
-            val bitmap = MediaStore.Images.Media.getBitmap(activity.contentResolver, uri)
-
-            val filePath = getRealPathFromURI(uri)
-
-            // Corregir la orientación del bitmap
-            val correctedBitmap = correctBitmapOrientation(bitmap, filePath)
-
-            return listOf(correctedBitmap)
+            val bitmap = MediaStore.Images.Media.getBitmap(activity.contentResolver, data?.data)
+            return listOf(bitmap)
         }
         throw Exception("ERROR_GETTING_IMAGE")
     }
-
-    private fun getRealPathFromURI(uri: Uri): String {
-        var filePath = ""
-        val cursor = activity.contentResolver.query(uri, null, null, null, null)
-        if (cursor != null) {
-            cursor.moveToFirst()
-            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
-            if (idx != -1) {
-                filePath = cursor.getString(idx)
-            }
-            cursor.close()
-        }
-        return filePath
-    }
-
-    private fun correctBitmapOrientation(bitmap: Bitmap, filePath: String): Bitmap {
-        val exif = try {
-            ExifInterface(filePath)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return bitmap
-        }
-
-        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-
-        val matrix = Matrix()
-        when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> matrix.postRotate(90f)
-            ExifInterface.ORIENTATION_ROTATE_180 -> matrix.postRotate(180f)
-            ExifInterface.ORIENTATION_ROTATE_270 -> matrix.postRotate(270f)
-        }
-
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-    }
-
     override fun getRequestCode(): Int{
         return REQUESTCODE
     }
