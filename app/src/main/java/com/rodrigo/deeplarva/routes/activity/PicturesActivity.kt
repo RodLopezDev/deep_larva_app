ad0eb89d17abbb0de00341f185cce1f16ee55f09
RodLopezDev <rodrigolopezdev@gmail.com>
2024-08-26 20:34:54 -0500

    fix: :bug: fixed issue about broadcast servcices

--- app/src/main/java/com/rodrigo/deeplarva/routes/activity/PicturesActivity.kt
+++ app/src/main/java/com/rodrigo/deeplarva/routes/activity/PicturesActivity.kt
@@ -61,6 +61,7 @@ class PicturesActivity: BoundedActivity(), IPictureViewListener, IBoundService {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPicturesBinding.inflate(layoutInflater)
