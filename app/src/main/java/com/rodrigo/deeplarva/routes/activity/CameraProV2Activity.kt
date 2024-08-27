82773676eb046a9e7a7522b7bb03a833a0ff20ee
RodLopezDev <rodrigolopezdev@gmail.com>
2024-08-27 00:40:01 -0500

    feat: :sparkles: implemented new camera features

--- app/src/main/java/com/rodrigo/deeplarva/routes/activity/CameraProV2Activity.kt
+++ app/src/main/java/com/rodrigo/deeplarva/routes/activity/CameraProV2Activity.kt
@@ -1,24 +1,22 @@
 package com.rodrigo.deeplarva.routes.activity
 
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import androidx.annotation.OptIn
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.ExperimentalZeroShutterLag
 import androidx.camera.core.ImageCapture
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.kylecorry.andromeda.camera.ImageCaptureSettings
+import com.kylecorry.andromeda.core.math.DecimalFormatter
 import com.kylecorry.andromeda.core.time.CoroutineTimer
-import com.kylecorry.andromeda.core.ui.setOnProgressChangeListener
-import com.kylecorry.andromeda.files.ExternalFileSystem
 import com.kylecorry.andromeda.files.LocalFileSystem
 import com.kylecorry.andromeda.haptics.HapticFeedbackType
 import com.kylecorry.andromeda.haptics.HapticMotor
-import com.kylecorry.andromeda.pickers.Pickers
 import com.kylecorry.luna.coroutines.CoroutineQueueRunner
 import com.kylecorry.luna.coroutines.onIO
 import com.kylecorry.luna.coroutines.onMain
@@ -26,7 +24,10 @@ import com.rodrigo.deeplarva.application.utils.Constants
 import com.rodrigo.deeplarva.databinding.ActivityCameraPro2Binding
 import com.rodrigo.deeplarva.helpers.PreferencesHelper
 import com.rodrigo.deeplarva.modules.camerapro2.infraestructure.SensitivityProvider
-import com.rodrigo.deeplarva.modules.camerapro2.ui.CustomUiUtils
+import com.rodrigo.deeplarva.routes.activity.observables.CameraV2Model
+import com.rodrigo.deeplarva.routes.activity.stores.CameraParameterStore
+import com.rodrigo.deeplarva.ui.widget.dialogs.SeekDialog
+import com.rodrigo.deeplarva.ui.widget.dialogs.SelectableDialog
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
@@ -39,15 +40,14 @@ class CameraProV2Activity: AppCompatActivity() {
     private lateinit var files: LocalFileSystem
     private lateinit var haptics: HapticMotor
 
+    private lateinit var viewModel: CameraV2Model
+    private lateinit var cameraStore: CameraParameterStore
+
     // Require state
-    private var focusPercent:Float? = null
-    private var iso: Int? = null
     private var sensitivities = emptyList<Int>()
     private var cameraStartCounter = 0
     private var isCameraRunning = false
     private var isCapturing = false
-    private var interval: Duration? = null
-    private var shutterSpeed: Duration? = null
     private var zoomRatio = 1f
 
     private val queue = CoroutineQueueRunner(1)
@@ -73,7 +73,9 @@ class CameraProV2Activity: AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        cameraStore = CameraParameterStore(this)
         deviceID = PreferencesHelper(this).getString(Constants.SHARED_PREFERENCES_DEVICE_ID) ?: ""
+        viewModel = ViewModelProvider(this)[CameraV2Model::class.java]
 
         binding = ActivityCameraPro2Binding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -82,6 +84,7 @@ class CameraProV2Activity: AppCompatActivity() {
         haptics = HapticMotor(this)
 
         onCreatedView()
+        onUpdate()
     }
     @OptIn(ExperimentalZeroShutterLag::class)
     override fun onResume() {
@@ -111,11 +114,6 @@ class CameraProV2Activity: AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun onCreatedView () {
-
-        binding.focus.setOnProgressChangeListener { progress, _ ->
-            focusPercent = progress / 100f
-        }
-
         binding.captureButton.setOnClickListener {
             takePhoto()
         }
@@ -123,47 +121,59 @@ class CameraProV2Activity: AppCompatActivity() {
         binding.iso.setOnClickListener {
             val sensitivityNames =
                 listOf("Auto") + sensitivities.map { it.toString() }
+            val defaultValue = sensitivities.indexOf(viewModel.iso.value) + 1
 
-            Pickers.item(
-                applicationContext,
-                "Iso",
-                sensitivityNames,
-                sensitivities.indexOf(iso) + 1,
-            ) {
-                it ?: return@item
-                iso = if (it == 0) {
+            val dialog = SelectableDialog(
+                items = sensitivityNames,
+                defaultIndexValue = defaultValue,
+                title = "Modificar ISO",
+                okButtonText = "OK",
+                cancelButtonText = "Cancel"
+            ) { selectedItem ->
+                val newIso = if (selectedItem == 0) {
                     null
                 } else {
-                    sensitivities[it - 1]
+                    sensitivities[selectedItem - 1]
+                }
+                viewModel.setIso(newIso)
+                if(newIso != null) {
+                    cameraStore.updateSensitivitySensor(newIso)
                 }
             }
+            dialog.show(supportFragmentManager, "ListSelectionDialog")
         }
 
         binding.shutterSpeed.setOnClickListener {
-            Pickers.number(
-                applicationContext,
-                "Shutter Speed",
-                default = shutterSpeed?.toMillis()?.div(1000f),
-                allowDecimals = true,
-                allowNegative = false,
-                hint = "Shutter Speed"
-            ) {
-                if (it != null) {
-                    shutterSpeed = Duration.ofMillis((it.toFloat() * 1000).toLong())
-                }
+            val initial = if(viewModel.shutterSpeed.value != null) {
+                viewModel.shutterSpeed.value!!.toMillis().toInt()
+            } else {
+                cameraStore.getCameraValues().shootSpeedMin
             }
+            val dialog = SeekDialog(
+                minValue = cameraStore.getCameraValues().shootSpeedMin,
+                maxValue = cameraStore.getCameraValues().shootSpeedMax,
+                initialValue = initial
+            ) { selectedValue ->
+                val newShutterSpeed = Duration.ofMillis((selectedValue.toFloat() * 1000).toLong())
+                viewModel.setShutterSpeed(newShutterSpeed)
+                cameraStore.updateShootSpeed(selectedValue)
+            }
+            dialog.show(supportFragmentManager, "IntervalPickerDialog")
         }
 
-        binding.interval.setOnClickListener {
-            CustomUiUtils.pickDuration(
-                applicationContext,
-                interval,
-                "Interval",
-                showSeconds = true
-            ) {
-                interval = it
-            }
-        }
+//        binding.interval.setOnClickListener {
+//            CustomUiUtils.pickDuration(
+//                applicationContext,
+//                viewModel.interval.value,
+//                "Interval",
+//                showSeconds = true
+//            ) {
+//                val newInterval = it
+//                if(newInterval != null) {
+//                    viewModel.setInterval(newInterval)
+//                }
+//            }
+//        }
 
         binding.camera.setOnZoomChangeListener {
             zoomRatio = binding.camera.camera?.zoom?.ratio ?: 1f
@@ -180,6 +190,7 @@ class CameraProV2Activity: AppCompatActivity() {
             )
         )
         isCameraRunning = true
+        defineInitialValues()
     }
 
     private fun restartCamera() {
@@ -222,38 +233,6 @@ class CameraProV2Activity: AppCompatActivity() {
             }
         }
     }
-
-    private suspend fun copyToMediaStore(file: File) = onIO {
-        // Save the file to the system media store
-        val resolver = applicationContext.contentResolver
-        val photoCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        } else {
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        }
-
-        val photoDetails = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, file.name)
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
-            put(MediaStore.Images.Media.IS_PENDING, 1)
-        }
-
-        val photoContentUri = resolver.insert(photoCollection, photoDetails) ?: return@onIO
-        val externalFiles = ExternalFileSystem(applicationContext)
-        externalFiles.outputStream(photoContentUri)?.use { output ->
-            file.inputStream().use { input ->
-                input.copyTo(output)
-            }
-        }
-
-        photoDetails.clear()
-        photoDetails.put(MediaStore.Audio.Media.IS_PENDING, 0)
-        resolver.update(photoContentUri, photoDetails, null, null)
-
-        pictures.add(file.absolutePath)
-        onCloseView()
-    }
     private fun onCloseView() {
         val returnIntent = Intent()
         if(pictures.isEmpty()) {
@@ -264,5 +243,50 @@ class CameraProV2Activity: AppCompatActivity() {
             setResult(RESULT_OK, returnIntent)
         }
         finish()
+    }
+
+    private fun onUpdate() {
+        viewModel.iso.observe(this, Observer {
+            val iso = it
+            binding.iso.text = iso?.toString() ?: "Auto"
+            binding.camera.camera?.setSensitivity(iso)
+        })
+        viewModel.shutterSpeed.observe(this, Observer {
+            val shutterSpeed = it
+            binding.shutterSpeed.text =
+                shutterSpeed?.let { DecimalFormatter.format(shutterSpeed.toMillis() / 1000f, 2) }
+                    ?: "Auto"
+            binding.camera.camera?.setExposureTime(shutterSpeed)
+            val previous = viewModel.previousShutterSpeed.value
+            viewModel.setPreviousShutterSpeed(shutterSpeed)
+            if (shutterSpeed != previous && previous != null && previous > Duration.ofMillis(250)) {
+                restartCamera()
+            }
+        })
+        viewModel.interval.observe(this, Observer {
+            val interval = it
+            binding.interval.text =
+                interval?.let { DecimalFormatter.format(interval.toMillis() / 1000f, 2) }
+                    ?: "Off"
+            if (interval != null) {
+                turnOffDuringInterval = interval > Duration.ofSeconds(2)
+                intervalometer.interval(interval)
+            } else {
+                val wasRunning = turnOffDuringInterval
+                intervalometer.stop()
+                hasPendingPhoto = false
+                turnOffDuringInterval = false
+                if (wasRunning) {
+                    restartCamera()
+                }
+            }
+        })
+    }
+    private fun defineInitialValues() {
+        val initialISO = cameraStore.getCameraValues().sensorSensitivity
+        val initialDuration = Duration.ofMillis((cameraStore.getCameraValues().shootSpeed.toFloat() * 1000).toLong())
+
+        viewModel.setIso(initialISO)
+        viewModel.setShutterSpeed(initialDuration)
     }
 }
